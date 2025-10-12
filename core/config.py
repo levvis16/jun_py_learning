db4ba0b1ac7010e52260039d6773bf25fedc0ec8
levvis16 <zoomcross2@gmail.com>
2025-10-12 23:50:45 +0300

    сделал crud

--- core/config.py
+++ core/config.py
@@ -1,9 +1,12 @@
-from os import getenv
+from pathlib import Path
 from pydantic_settings import BaseSettings
 
+BASE_DIR = Path(__file__).parent.parent
+
 class Setting(BaseSettings):
-    db_url: str = 'sqlite+aiosqlite:///./db.sqlite3'
-    db_echo: bool = False
+    api_v1_prefix: str = '/api/v1'
+    db_url: str = f'sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3'
+    db_echo: bool = True
 
 settings = Setting()
 
